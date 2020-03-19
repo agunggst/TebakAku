67c3b0402d72d9483d70627c17011e9a36806539
adhiyatmarama <adhiyatma.pramayoga@gmail.com>
2020-03-19 20:15:47 +0700

    Membuat halaman home: 1. Membuat form input nama pada halaman home untuk memulai game 2. Membuat variabel pada state di store 3. Membuat mutasi untuk re-assign state

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,8 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    myName: '',
+    socket: null,
   },
   mutations: {
+    setMyName(state, data) {
+      state.myName = data
+    },
+    setSocket(state, data) {
+      state.socket = data
+    },
   },
   actions: {
   },
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>TebakAku</h1>
+    <form @submit.prevent="start">
+      <input type="text" placeholder="Enter your name" v-model="playername">
+      <button type="submit" :disabled="playerName === ''">Let's Go!</button>
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapMutations } from 'vuex';
+import io from 'socket.io-client';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      playerName: '',
+    };
+  },
+  computed: {
+  },
+  methods: {
+    ...mapMutations(['setSocket', 'setMyName']),
+    start() {
+      this.setMyName(this.playerName);
+      if(this.$store.state.socket === null){
+        let socket = io('http://localhost:3000');
+        this.setSocket(socket);
+      }
+      localStorage.setItem('name', this.playerName)
+      this.$router.push('/lobby')
+    }
   }
 }
 </script>
